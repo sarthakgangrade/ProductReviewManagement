f56bef765e9e9194d17ba9495ede7590987bbc5c
sarthakgangrade <sarthakgangrade0@gmail.com>
2022-01-08 07:00:32 +0530

    create product review class add 25 values of product review data

---
+++ productRewiewManagement/productRewiewManagement/ProductReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productRewiewManagement
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
