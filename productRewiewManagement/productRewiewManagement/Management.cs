45ad93e0e4380423c46b9b9c657c41cb5be8a4ba
sarthakgangrade <sarthakgangrade0@gmail.com>
2022-01-08 07:16:47 +0530

    retrieve top three records from the list

---
+++ productRewiewManagement/productRewiewManagement/Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productRewiewManagement
+{
+    internal class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> productlist)
+        {
+            var record = (from ProductReview in productlist orderby ProductReview.Rating descending
+                          select ProductReview).Take(3);
+            foreach (var r in record)
+            {
+                Console.WriteLine("PRODUCT_ID-" + r.ProductID + "  " + "USER_ID-" + r.UserID + "  " + "RATING-" + r.Rating + "  " + "REVIEW-" + r.Review);
+            }
+        }
+    }
+}
