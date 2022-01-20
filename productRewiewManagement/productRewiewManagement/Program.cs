026df700c40eb17e04cdabe442a9a9dfbd68c8bc
sarthakgangrade <sarthakgangrade0@gmail.com>
2022-01-20 17:51:28 +0530

    records from data table where the review is nice

--- productRewiewManagement/productRewiewManagement/Program.cs
+++ productRewiewManagement/productRewiewManagement/Program.cs
@@ -18,13 +18,13 @@ List<ProductReview> list = new List<ProductReview>()
             new ProductReview() { ProductID = 12, UserID = 33, Rating = 4, Review = "BAD" ,isLike= false},
             new ProductReview() { ProductID = 13, UserID = 34, Rating = 3, Review = "BAD" ,isLike= false},
             new ProductReview() { ProductID = 14, UserID = 35, Rating = 2, Review = "BAD" ,isLike= false},
-            new ProductReview() { ProductID = 15, UserID = 36, Rating = 7.5, Review = "GOOD" ,isLike= true},
+            new ProductReview() { ProductID = 15, UserID = 36, Rating = 7.5, Review = "nice" ,isLike= true},
             new ProductReview() { ProductID = 16, UserID = 37, Rating = 7, Review = "GOOD" ,isLike= false},
             new ProductReview() { ProductID = 17, UserID = 38, Rating = 6.8, Review = "AVERAGE" ,isLike= false},
             new ProductReview() { ProductID = 18, UserID = 39, Rating = 6.5, Review = "AVERAGE" ,isLike= false},
             new ProductReview() { ProductID = 19, UserID = 40, Rating = 6, Review = "AVERAGE" ,isLike= false},
             new ProductReview() { ProductID = 20, UserID = 41, Rating = 5.5, Review = "AVERAGE" ,isLike= false},
-            new ProductReview() { ProductID = 21, UserID = 42, Rating = 5, Review = "BAD" ,isLike= true},
+            new ProductReview() { ProductID = 21, UserID = 42, Rating = 5, Review = "nice" ,isLike= true},
             new ProductReview() { ProductID = 22, UserID = 43, Rating = 4, Review = "BAD" ,isLike= false},
             new ProductReview() { ProductID = 25, UserID = 44, Rating = 3, Review = "BAD" ,isLike= true},
             new ProductReview() { ProductID = 25, UserID = 45, Rating = 2, Review = "BAD" ,isLike= false},
@@ -42,5 +42,6 @@ Management management = new Management();
 //management.RetrieveProductIDAndReview(list);
 //management.Skip(list);
 management.CreateDataTable(list);
-management.Islike_true(list);
-management.AverageRating(list);
+//management.Islike_true(list);
+//management.AverageRating(list);
+management.Reviewisnice(list);
